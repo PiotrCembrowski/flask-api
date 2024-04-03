d071bf999c384b06c0826146eb1cd1608c940765
Piotr Cembrowski <losttape@gmail.com>
2024-04-03 13:59:50 +0200

    adding some authentication code lines

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
-import json
-
 from flask import Flask, redirect, url_for, render_template, flash, session, \
     current_app, request, abort
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import UUID
 from flask_migrate import Migrate
-from flask_login import LoginManager, UserMixin, login_user, logout_user,\
-current_user
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
 from flask_cors import CORS
 from dotenv import load_dotenv
 from urllib.parse import urlencode
-import os, secrets, requests, uuid
+import os, secrets, requests, uuid, functools
 from werkzeug.utils import secure_filename
 
+load_dotenv()
 UPLOAD_FOLDER = './static/uploads'
 ALLOWED_EXTENSIONS = {'txt', 'pdf'}
 app = Flask(__name__)
-app.secret_key = os.getenv('SUPER_SECRET_KEY')
+app.config['SECRET_KEY'] = os.getenv('SUPER_SECRET_KEY')
+app.config['SECURITY_PASSWORD_SALT'] = os.getenv('SECURITY_PASSWORD_SALT')
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:root@localhost:5432/sf"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db = SQLAlchemy(app)
@@ -25,16 +24,160 @@ migrate = Migrate(app, db)
 api = Api(app)
 CORS(app, supports_credentials=True)
 
-# USER
+login = LoginManager(app)
+login.init_app(app)
+login.login_view = 'index'
 
-class User():
-    def __init__(self, id, username, password):
-        self.id = id
+# OAUTH
+
+
+app.config['OAUTH2_PROVIDERS'] = {
+    # Google OAuth 2.0 documentation:
+    # https://developers.google.com/identity/protocols/oauth2/web-server#httprest
+    'google': {
+        'client_id': os.getenv('GOOGLE_CLIENT_ID'),
+        'client_secret': os.getenv('GOOGLE_CLIENT_SECRET'),
+        'authorize_url': 'https://accounts.google.com/o/oauth2/auth',
+        'token_url': 'https://accounts.google.com/o/oauth2/token',
+        'userinfo': {
+            'url': 'https://www.googleapis.com/oauth2/v3/userinfo',
+            'email': lambda json: json['email'],
+        },
+        'scopes': ['https://www.googleapis.com/auth/userinfo.email'],
+    },
+}
+
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4())
+    username = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), unique=True, nullable=False)
+
+
+    def __init__(self, username, email):
         self.username = username
-        self.password = password
+        self.email = email
 
-    def __str__(self):
-        return f"User ID: {self.id}"
+
+
+@login.user_loader
+def load_user(id_):
+    return db.session.get(User, id_)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('index'))
+
+
+@app.route('/authorize/<provider>')
+def oauth2_authorize(provider):
+    if not current_user.is_anonymous:
+        print('not annonim')
+        return redirect(url_for('index'))
+
+    provider_data = current_app.config['OAUTH2_PROVIDERS'].get(provider)
+    if provider_data is None:
+        abort(404)
+
+    # generate a random string for the state parameter
+    session['oauth2_state'] = secrets.token_urlsafe(16)
+
+    # create a query string with all the OAuth2 parameters
+    qs = urlencode({
+        'client_id': provider_data['client_id'],
+        'redirect_uri': url_for('oauth2_callback', provider=provider,
+                                _external=True),
+        'response_type': 'code',
+        'scope': ' '.join(provider_data['scopes']),
+        'state': session['oauth2_state'],
+    })
+
+    # redirect the user to the OAuth2 provider authorization URL
+    return redirect(provider_data['authorize_url'] + '?' + qs)
+
+
+
+@app.route('/callback/<provider>')
+def oauth2_callback(provider):
+    if not current_user.is_anonymous:
+        return redirect(url_for('index'))
+
+    provider_data = current_app.config['OAUTH2_PROVIDERS'].get(provider)
+    if provider_data is None:
+        abort(404)
+
+    # if there was an authentication error, flash the error messages and exit
+    if 'error' in request.args:
+        for k, v in request.args.items():
+            if k.startswith('error'):
+                flash(f'{k}: {v}')
+        return redirect(url_for('index'))
+
+    # make sure that the state parameter matches the one we created in the
+    # authorization request
+    if request.args['state'] != session.get('oauth2_state'):
+        abort(401)
+
+    # make sure that the authorization code is present
+    if 'code' not in request.args:
+        abort(401)
+
+    # exchange the authorization code for an access token
+    response = requests.post(provider_data['token_url'], data={
+        'client_id': provider_data['client_id'],
+        'client_secret': provider_data['client_secret'],
+        'code': request.args['code'],
+        'grant_type': 'authorization_code',
+        'redirect_uri': url_for('oauth2_callback', provider=provider,
+                                _external=True),
+    }, headers={'Accept': 'application/json'})
+    if response.status_code != 200:
+        abort(401)
+
+    
+    oauth2_token = response.json().get('access_token')
+
+    if not oauth2_token:
+        abort(401)
+
+
+    # use the access token to get the user's email address
+    response = requests.get(provider_data['userinfo']['url'], headers={
+        'Authorization': 'Bearer ' + oauth2_token,
+        'Accept': 'application/json',
+    })
+    if response.status_code != 200:
+        abort(401)
+
+    
+
+    email = provider_data['userinfo']['email'](response.json())
+
+    # find or create the user in the database
+    user = db.session.scalar(db.select(User).where(User.email == email))
+    if user is None:
+        user = User(email=email, username=email.split('@')[0])
+        db.session.add(user)
+        db.session.commit()
+
+    # log the user in
+    login_user(user, remember=False, duration=None, force=False, fresh=True)
+    print(current_user)
+    return redirect(url_for('index'))
+
+
+
+
 
 # Files
 class FilesList(db.Model):
@@ -42,34 +185,43 @@ class FilesList(db.Model):
 
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4())
     name = db.Column(db.String())
+    user_id = db.Column(UUID(as_uuid=True))
 
-    def __init__(self, name):
+    def __init__(self, name, user_id):
         self.name = name
+        self.user_id = user_id
 
     def __repr__(self):
         return f"<File {self.name}>"
 
+
 @app.route('/fileslists', methods=['POST', 'GET'])
 def handle_fileslists():
-    if request.method == 'POST':
-        if request.is_json:
-            data = request.get_json()
-            new_list = FilesList(name=data['name'])
-            db.session.add(new_list)
-            db.session.commit()
-            return {"message": f"list {new_list.name} has been created successfully."}
-        else:
-            return {"message": "The request payload is not a JSON format"}
-    
-    elif request.method == 'GET':
-        lists = FilesList.query.all()
-        results = [
-            {
-                "id": list.id,
-                "name": list.name,
-            } for list in lists
-        ]
-        return {"count": len(results), "lists": results}
+        if request.method == 'POST':
+            if not current_user.is_anonymous:
+                if request.is_json:
+                    data = request.get_json()
+                    new_list = FilesList(name=data['name'], user_id=data['user_id'])
+                    db.session.add(new_list)
+                    db.session.commit()
+                    return {"message": f"list {new_list.name} has been created successfully."}
+                else:
+                    return {"message": "The request payload is not a JSON format"}
+
+        elif request.method == 'GET':
+            if current_user.is_authenticated:
+                print(current_user.id)
+                lists = FilesList.query.all()
+                results = [
+                    {
+                        "id": list.id,
+                        "name": list.name,
+                        "user_id": list.user_id
+                    } for list in lists
+                ]
+                return {"count": len(results), "lists": results}
+
+
 
 @app.route('/fileslists/<id>', methods=['GET', 'PUT', 'DELETE'])
 def handle_fileslists_by_id(id):
@@ -81,7 +233,7 @@ def handle_fileslists_by_id(id):
             "name": list.name,
         }
         return {"list": response}
-    
+
     elif request.method == 'PUT':
         data = request.get_json()
         list.id = data['id']
@@ -89,7 +241,7 @@ def handle_fileslists_by_id(id):
         db.session.add(list)
         db.session.commit()
         return {"message": f"list {list.name} successfully updated"}
-    
+
     elif request.method == "DELETE":
         db.session.delete(list)
         db.session.commit()
@@ -97,7 +249,6 @@ def handle_fileslists_by_id(id):
     
 
 # FILE
-
 class File(db.Model):
     __tablename__ = 'file'
 
@@ -137,6 +288,7 @@ def handle_upload():
             return {"message": f"You've uploaded {file.filename}"}
         else:
             return {"message": "Error occurred."}
+
 
 
 @app.route('/files', methods=['POST', 'GET'])
@@ -191,147 +343,9 @@ def handle_files_by_id(id):
         return {"message": f"List {file.name} successfully deleted."}
 
 
-# OAUTH
-    
-load_dotenv()
-
-app.config['OAUTH2_PROVIDERS'] = {
-    # Google OAuth 2.0 documentation:
-    # https://developers.google.com/identity/protocols/oauth2/web-server#httprest
-    'google': {
-        'client_id': os.getenv('GOOGLE_CLIENT_ID'),
-        'client_secret': os.getenv('GOOGLE_CLIENT_SECRET'),
-        'authorize_url': 'https://accounts.google.com/o/oauth2/auth',
-        'token_url': 'https://accounts.google.com/o/oauth2/token',
-        'userinfo': {
-            'url': 'https://www.googleapis.com/oauth2/v3/userinfo',
-            'email': lambda json: json['email'],
-        },
-        'scopes': ['https://www.googleapis.com/auth/userinfo.email'],
-    },
-
-}
-
-login = LoginManager(app)
-login.login_view = 'index'
-
-
-
-class User(UserMixin, db.Model):
-    __tablename__ = 'users'
-    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4())
-    username = db.Column(db.String(64), nullable=False)
-    email = db.Column(db.String(64), nullable=True)
-@login.user_loader
-def load_user(id):
-    return db.session.get(User, id)
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    flash('You have been logged out.')
-    return redirect(url_for('index'))
-
-
-
-
-
-@app.route('/authorize/<provider>')
-def oauth2_authorize(provider):
-    if not current_user.is_anonymous:
-        return redirect(url_for('index'))
-
-    provider_data = current_app.config['OAUTH2_PROVIDERS'].get(provider)
-    if provider_data is None:
-        abort(404)
-
-    # generate a random string for the state parameter
-    session['oauth2_state'] = secrets.token_urlsafe(16)
-
-    # create a query string with all the OAuth2 parameters
-    qs = urlencode({
-        'client_id': provider_data['client_id'],
-        'redirect_uri': url_for('oauth2_callback', provider=provider,
-                                _external=True),
-        'response_type': 'code',
-        'scope': ' '.join(provider_data['scopes']),
-        'state': session['oauth2_state'],
-    })
-
-    # redirect the user to the OAuth2 provider authorization URL
-    return redirect(provider_data['authorize_url'] + '?' + qs)
-
-
-@app.route('/callback/<provider>')
-def oauth2_callback(provider):
-    if not current_user.is_anonymous:
-        return redirect(url_for('index'))
-
-    provider_data = current_app.config['OAUTH2_PROVIDERS'].get(provider)
-    if provider_data is None:
-        abort(404)
-
-    # if there was an authentication error, flash the error messages and exit
-    if 'error' in request.args:
-        for k, v in request.args.items():
-            if k.startswith('error'):
-                flash(f'{k}: {v}')
-        return redirect(url_for('index'))
-
-    # make sure that the state parameter matches the one we created in the
-    # authorization request
-    if request.args['state'] != session.get('oauth2_state'):
-        abort(401)
-
-    # make sure that the authorization code is present
-    if 'code' not in request.args:
-        abort(401)
-
-    # exchange the authorization code for an access token
-    response = requests.post(provider_data['token_url'], data={
-        'client_id': provider_data['client_id'],
-        'client_secret': provider_data['client_secret'],
-        'code': request.args['code'],
-        'grant_type': 'authorization_code',
-        'redirect_uri': url_for('oauth2_callback', provider=provider,
-                                _external=True),
-    }, headers={'Accept': 'application/json'})
-    if response.status_code != 200:
-        abort(401)
-    oauth2_token = response.json().get('access_token')
-    if not oauth2_token:
-        abort(401)
-
-    # use the access token to get the user's email address
-    response = requests.get(provider_data['userinfo']['url'], headers={
-        'Authorization': 'Bearer ' + oauth2_token,
-        'Accept': 'application/json',
-    })
-    if response.status_code != 200:
-        abort(401)
-
-    email = provider_data['userinfo']['email'](response.json())
-
-    # find or create the user in the database
-    user = db.session.scalar(db.select(User).where(User.email == email))
-    if user is None:
-        user = User(email=email, username=email.split('@')[0])
-        db.session.add(user)
-        db.session.commit()
-
-    # log the user in
-    login_user(user)
-    return redirect(url_for('index'))
 
 with app.app_context():
     db.create_all()
 
-
 if __name__ == "__main__":
-    app.run(port=8080, debug=True)
+    app.run(port=5000, debug=True)
