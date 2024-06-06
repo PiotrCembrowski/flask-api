5c225deaefdca34b96d820409eae61c2cdf20483
losttapepoetry <81425746+PiotrCembrowski@users.noreply.github.com>
2024-06-06 13:43:08 +0200

    function for create request added

--- test_api.py
+++ test_api.py
@@ -2,15 +2,17 @@ import requests
 
 ENDPOINT = 'http://127.0.0.1:5000/files'
 
-response = requests.get(ENDPOINT)
-print(response)
-
-data = response.json()
-print(data)
-
-status_code = response.status_code
-print(status_code)
-
 def test_can_call_endpoint():
     response = requests.get(ENDPOINT)
+    assert response.status_code == 200
+    
+    
+def text_can_create_file():
+    payload = {
+        "content": "My test content",
+        "user_id": "test_user",
+        "task_id": "test_file_id",
+        "is_done": False
+    }
+    response = requests.post(ENDPOINT + '/files', json=payload)
     assert response.status_code == 200
