df95d153f141c1b13b97184065869cd398e0b5ab
losttapepoetry <81425746+PiotrCembrowski@users.noreply.github.com>
2024-06-06 12:41:16 +0200

    response,data and status_code added

--- test_api.py
+++ test_api.py
@@ -1,1 +1,12 @@
+import requests
+
 ENDPOINT = 'http://127.0.0.1:5000/files'
+
+response = requests.get(ENDPOINT)
+print(response)
+
+data = response.json()
+print(data)
+
+status_code = response.status_code
+print(status_code)
